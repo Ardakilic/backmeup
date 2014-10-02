89a7e90eb12f57844b7b7b399da2ccbcc9e4cc1e
Arda Kilicdagi <ardakilicdagi@gmail.com>
2014-10-02 16:17:08 +0300

    added the script and readme

--- README.md
+++ README.md
@@ -1,4 +1,107 @@
-backmeup
-========
+BackMeUp
+=========
 
-BackMeUp: An automated MySQL databases and files backup solution on Linux Machines using Dropbox
+BackMeUp is an automated MySQL databases and files backup solution on Linux Machines using Dropbox as remote service.
+
+
+```
+  ____             _    __  __      _    _       
+ |  _ \           | |  |  \/  |    | |  | |      
+ | |_) | __ _  ___| | _| \  / | ___| |  | |_ __  
+ |  _ < / _, |/ __| |/ / |\/| |/ _ \ |  | | '_ \ 
+ | |_) | (_| | (__|   <| |  | |  __/ |__| | |_) |
+ |____/ \__,_|\___|_|\_\_|  |_|\___|\____/| .__/ 
+                                          | |    
+                                          |_|    
+```
+
+Why?
+--------------
+I'm managing my own server, and I wanted to have a simple and easy tool to backup my database and all VirtualHost files, and to save them into a remote server with cron.
+
+What This Script Does
+--------------
+This script does some simple tasks:
+* The script dumps all of your MySQL databases as separate files.
+* The script backs up all of your Web files (e.g: root of all of your virtual hosts).
+* The script compresses your web-root and databases to a single archive.
+* The script uploads the compressed archive into a folder in Dropbox.
+* After the upload, the script cleans the temporary files (dumps, the archive itself). 
+* The script makes sure that you always have the newest Dropbox-uploader script.
+
+You may easily add this script to your crontab, and 
+
+Version
+--------------
+
+0.1
+
+Requirements
+--------------
+* `curl` - To download the dropbox-uploader script and to upload the backup to Dropbox.
+* `mysql-cli` - To list databases.
+* `mysqldump` - To dump databases (in most cases, it comes with `mysql-cli`).
+
+
+Don't Have a Dropbox Account?
+--------------
+Don't worry :)
+
+Just click on [this link](https://db.tt/A4QRGuD) to start Dropbox with a bonus space. With my referral, we both earn bonus space.
+
+
+Configuration Values
+--------------
+After downloading the script, before running, you must edit your configuration values:
+
+```sh
+TIMEZONE="Europe/Istanbul" #Your timezone, for a better timestamp in archived filenames
+DBUSER="root" #MySQL user that can dump all databases
+DBPASSWORD="" #MySQL password
+FILESROOT="/var/www" #root of your (virtual) hosting files, E.g: For apache, it is /var/www, for nginx, it's /usr/share/nginx/html "WITHOUT TRAILING SLASH"
+BASEFOLDER="/tmp" #Temporary folder to create database dump folder
+DROPBOXFOLDER="backmeup" #your backup folder that'll be created on Dropbox
+```
+
+Installation
+--------------
+
+* Run this command first:
+  ```
+  curl https://raw.githubusercontent.com/Ardakilic/backmeup/master/backmeup.sh -O backmeup.sh
+  ```
+* Now, edit the configuration values as stated [here](#configuration-values)
+* Make the file executable and only accessible by root:
+  ```
+  chown root:root backmeup.sh
+  chmod 700 backmeup.sh
+  ```
+
+Usage
+--------------
+
+* Execute the configured script:
+  ```
+  ./backmeup.sh
+  ```
+* If this is the first attempt to running, Dropbox-Uploader will ask for an APP key and secret. You should create an appliction, provide these values and click on provided authorization link (Don't worry, the Dropbox-uploader has a nice wizard which guides you, can't be easier). After you've authorized, re-run the script using `.backmeup.sh`
+* If everything went well, in a couple of minutes, you should see your database dumped into the 
+
+Important Notice
+--------------
+This script saves MySQL Root password inside, but it's only accessible by root. In any ways, use it at your own risk. I'm not holding any responsibilities for any damage that this script may do (which shouldn't).
+
+Special Thanks
+--------------
+@andreafabrizi for maintaining the [Dropbox-Uploader](https://github.com/andreafabrizi/Dropbox-Uploader) script.
+
+TODO
+--------------
+* Tests on CentOS, Arch etc.
+* Copy.com integration
+* Increased security?
+
+License
+--------------
+
+MIT
---
+++ backmeup.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+echo '-------------------------------------------------'
+echo "  ____             _    __  __      _    _       "
+echo " |  _ \           | |  |  \/  |    | |  | |      "
+echo " | |_) | __ _  ___| | _| \  / | ___| |  | |_ __  "
+echo " |  _ < / _, |/ __| |/ / |\/| |/ _ \ |  | | '_ \ "
+echo " | |_) | (_| | (__|   <| |  | |  __/ |__| | |_) |"
+echo " |____/ \__,_|\___|_|\_\_|  |_|\___|\____/| .__/ "
+echo "                                          | |    "
+echo "                                          |_|    "
+echo '-------------------------------------------------'
+echo ''
+echo '-------------------------------------------------'
+echo '| Author: Arda Kilicdagi'
+echo '| https://github.com/ardakilic/backmeup/'
+echo '-------------------------------------------------'
+echo ''
+
+
+#CONFIGURATION PARAMETERS
+TIMEZONE="Europe/Istanbul" #Your timezone, for a better timestamp in archived filenames
+DBUSER="root" #MySQL user that can dump all databases
+DBPASSWORD="" #MySQL password
+FILESROOT="/var/www" #root of your (virtual) hosting files, E.g: For apache, it is /var/www, for nginx, it's /usr/share/nginx/html "WITHOUT THE END TRAILING SLASH"
+BASEFOLDER="/tmp" #Temporary folder to create database dump folder (a subfolder will be created to this folder upon dumping)
+DROPBOXFOLDER="backmeup" #your backup folder that'll be created on Dropbox
+
+########################################################
+# DO NOT EDIT BELOW UNLESS YOU KNOW WHAT YOU'RE DOING!
+########################################################
+
+#Cleanup Function
+function cleanup {
+    rm -rf $1/backmeup*
+}
+
+#rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
+
+#needed for file and folder names
+THEDATE=`TZ=$TIMEZONE date +%Y-%m-%d_%H.%M.%S`
+
+INSTALLABLE="yes"
+DROPBOX_CONFIGURED="yes"
+ERRORMSGS=()
+if ! which curl > /dev/null;
+    then
+    INSTALLABLE="nope"
+    ERRORMSGS+=('| You must install curl to run this script')
+fi
+if ! which tar > /dev/null;
+    then
+    INSTALLABLE="nope"
+    ERRORMSGS+=('| You must install tar to run this script')
+fi
+if ! which mysql > /dev/null;
+    then
+    INSTALLABLE="nope"
+    ERRORMSGS+=('| You must install mysql to run this script')
+fi
+if ! which mysqldump > /dev/null;
+    then
+    INSTALLABLE="nope"
+    ERRORMSGS+=('| You must install mysqldump to run this script')
+fi
+
+
+
+if [[ "$INSTALLABLE" == "yes" ]]
+then
+    
+    #pre-cleanup
+    cleanup $BASEFOLDER
+    #folder for new backup
+    SQLFOLDER=backmeup-databases-$THEDATE
+    SQLFOLDERFULL=$BASEFOLDER/$SQLFOLDER
+    mkdir $SQLFOLDERFULL
+
+    #Now let's fetch Dropbox Uploader
+    #https://github.com/andreafabrizi/Dropbox-Uploader
+    #to make sure it's always the newest version, first let's delete and fetch it
+    cd $HOME
+    echo '| Fetching the newest Dropbox-Uploader from repository...'
+    rm -rf dropbox_uploader.sh
+    curl -s https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh -o dropbox_uploader.sh
+    echo '| Done!'
+    echo '-------------------------------------------------'
+    #make it executable
+    chmod +x dropbox_uploader.sh
+
+    #Is Dropbox-Uploader configured?
+    if [ ! -f $HOME/.dropbox_uploader ]; 
+        then
+        echo '| You must configure the Dropbox first!'
+        echo '| Please run ./dropbox_uploader.sh and follow the instructions.'
+        echo '| After that, re-run this script'
+    else
+        #everything okay, now let's start dumping
+        echo '| Dumping Databases...'
+        echo '|'
+        #Let's start dumping the databases
+        databases=`mysql --user=$DBUSER -p$DBPASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+        for db in $databases; do
+            if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+                echo "| Dumping database: $db"
+                mysqldump -u$DBUSER -p$DBPASSWORD $db > $SQLFOLDERFULL/$THEDATE.$db.sql
+            fi
+        done
+        echo '|'
+        echo '| Done!'
+        echo '|'
+
+        #Now let's compress
+        FILENAME="backmeup-$THEDATE.tar.gz"
+        echo '| Now compressing the backup...'
+        tar -zcvf $FILENAME -C $FILESROOT . -C $BASEFOLDER $SQLFOLDER/ > /dev/null
+        echo '|'
+        echo "| Done! The backup's name is: $FILENAME"
+        echo '|'
+
+        #Now, let's upload to Dropbox:
+        echo '| Creating the directory and uploading to Dropbox...'
+        ./dropbox_uploader.sh mkdir $DROPBOXFOLDER
+        ./dropbox_uploader.sh upload $FILENAME $DROPBOXFOLDER
+        echo '|'
+        echo '| Done!'
+        echo '|'
+
+        echo '| Cleaning up..'
+        #Now let's cleanup
+        rm -r $FILENAME
+        cleanup $BASEFOLDER
+        echo '|'
+        echo "| Done! You should now see your backup '$FILENAME' inside the '$DROPBOXFOLDER' in your Dropbox"
+
+    fi
+
+else
+    echo '| ERROR:'
+    for i in "${ERRORMSGS[@]}"
+        do
+           echo $i
+        done
+fi
+
+echo '-------------------------------------------------'
+
+#Let's clean up just in case
+cleanup $BASEFOLDER
